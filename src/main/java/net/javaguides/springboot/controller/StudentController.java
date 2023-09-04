d6ed2535766ac9d07c8da37002dfb3ca85a0cf0e
wpghks <wpghks56@naver.com>
2023-09-04 11:14:25 +0900

    [feature] student & students 추가

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,12 +1,24 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    @GetMapping("/student")
     public Student getStudent() {
         return new Student(1, "Ramesh", "Fadatare");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1, "Ramesh", "Fadatare"),
+                new Student(2, "Tony", "Stark")
+        );
     }
 }
